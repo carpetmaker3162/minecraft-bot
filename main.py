6ec5276be3330d5f1a4cb544b264c58e1e33cae3
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2024-03-12 09:54:08 -0400

    add working pvp

--- main.py
+++ main.py
@@ -1,50 +1,9 @@
 import sys
-from javascript import require, On, Once, console
+from javascript import require, On
 
-mineflayer = require("mineflayer", "latest")
+mineflayer = require("mineflayer")
 pathfinder = require('mineflayer-pathfinder')
-
-"""
-// This is an example that uses mineflayer-pathfinder to showcase how simple it is to walk to goals
-
-const mineflayer = require('mineflayer')
-const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
-
-if (process.argv.length < 4 || process.argv.length > 6) {
-  console.log('Usage : node gps.js <host> <port> [<name>] [<password>]')
-  process.exit(1)
-}
-
-const bot = mineflayer.createBot({
-  host: process.argv[2],
-  port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'gps',
-  password: process.argv[5]
-})
-
-const RANGE_GOAL = 1 // get within this radius of the player
-
-bot.loadPlugin(pathfinder)
-
-bot.once('spawn', () => {
-  const defaultMove = new Movements(bot)
-
-  bot.on('chat', (username, message) => {
-    if (username === bot.username) return
-    if (message !== 'come') return
-    const target = bot.players[username]?.entity
-    if (!target) {
-      bot.chat("I don't see you !")
-      return
-    }
-    const { x: playerX, y: playerY, z: playerZ } = target.position
-
-    bot.pathfinder.setMovements(defaultMove)
-    bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
-  })
-})
-"""
-
+pvp = require('mineflayer-pvp').plugin
 
 if len(sys.argv) > 3:
     host = sys.argv[1]
@@ -64,9 +23,15 @@ bot = mineflayer.createBot({
 mcData = require('minecraft-data')(bot.version)
 
 bot.loadPlugin(pathfinder.pathfinder)
+bot.loadPlugin(pvp)
 
-@On(bot, "chat")
+attacking = False
+ignored_usernames = []
+
+@On(bot, 'chat')
 def handle_chat(this, username, message, *args):
+    global attacking
+    
     if username == bot.username:
         return
 
@@ -87,3 +52,36 @@ def handle_chat(this, username, message, *args):
 
         bot.pathfinder.setMovements(movements)
         bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, 1))
+    
+    elif cmd == '.attack':
+        attacking = not attacking
+        bot.chat(f'now {"attacking muahaha" if attacking else "no longer attacking"}')
+
+    elif cmd == '.hp':
+        bot.chat(f'On {bot.health} hp')
+    
+    elif cmd == '.ignore':
+        if len(cmdargs) > 0:
+            ignore_name = cmdargs[0]
+            if ignore_name in ignored_usernames:
+                ignored_usernames.remove(ignore_name)
+                bot.chat(f'now no longer ignoring {ignore_name}')
+            else:
+                ignored_usernames.append(ignore_name)
+                bot.chat(f'now ignoring {ignore_name}')
+
+@On(bot, 'physicsTick')
+def handle_physics_tick(this):
+    global ignored_usernames
+    global attacking
+    
+    # filter seems to be broken
+    filter = lambda a: (a.type == 'mob' 
+                        and a.position.distanceTo(bot.entity.position) <= 32 
+                        and a.displayName != 'Armor Stand' 
+                        and a.username not in ignored_usernames)
+    
+    entity = bot.nearestEntity(filter)
+    if entity and attacking:
+        print(repr(entity.username), ignored_usernames)
+        bot.pvp.attack(entity)
